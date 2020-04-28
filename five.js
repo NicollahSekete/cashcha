881882409975f74f6d1404e2d4ed47f4e974532f
NicollahSekete <nicollahtsekete@gmail.com>
2020-04-28 14:16:02 +0100

    second commit

--- five.js
+++ five.js
@@ -1,15 +1,37 @@
 //Change the user pin with validation 
 
-while(true) {
-	let userInput = prompt(`enter new password`);
-	let newPass = ''
-	console.log(newPass);
-	if (userInput ==newPass.value.length >=4 ) {
-		alert("password saved");
-		break;
-	} else {
+//while(true) {
+	//let userInput = prompt(`enter new password`);
+	//let newPass = ''
+	//console.log(newPass);
+	//if (userInput ==newPass.value.length >=4 ) {
+	//	alert("password saved");
+	//	break;
+	//} else {
 		
-		alert("please enter valid password");
-	}
+	//	alert("please enter valid password");
+	//}
+//}
+//console.log('finish');
+
+
+let counter = 0
+const toChangePin = (pin) => {
+    pin = prompt("Please enter pin")
+    while (true) {
+        if (pin == "9999") {
+            alert("correct pin")
+            newPin = prompt("Enter New Pin")
+            return alert("Your pin has changed")
+        }
+        else if (pin != "9999" && counter < 3) {
+                alert("incorrect pin")
+                counter++
+        }
+        else if (pin != "9999" && counter == 3) {
+            alert("to many attempts")
+            break
+        }
+    }
 }
-console.log('finish');
+toChangePin()
--- seven.js
+++ seven.js
@@ -1,16 +1,29 @@
 //Statement of actions of the transactions occurred. 
-let balance = 3000;
-let checkBalance = true;
-//let isActive = true;
+const todaysTransactions = (day) => {
+    day = prompt("What day would you like to see the transactions from?")
+    if (day == "monday"){
+    alert( "Your current account had £5000")
+     }
+     else if (day == "tuesday"){
+       alert ("Your current account had £3400")
 
-if (checkBalance === true) {
-    if (balance > 0) {
-        console.log("Your balance is £" + balance + ".");
-        } //else if (isActive === true) 
-        {
-            if (balance === 0) {
-                console.log("Your account is empty");
-            }
-        }
-
+    }else if (day == "Wednesday"){
+        alert ("Your current account had £400")
+     }
+     else if (day == "Thursday"){
+        alert ("Your current account had £5600")
+     }else if (day == "friday"){
+        alert ("You were overdrawn")
+     }
+     else if (day == "saturday"){
+        alert ("Your account was overdrawn and charged £40")
+     }
+     else if (day == "sunday"){
+        alert ("Your current account had £200")
+     }
+   else {
+        alert ("That is not a day on this planet")
     }
+}
+    
+     todaysTransactions()
