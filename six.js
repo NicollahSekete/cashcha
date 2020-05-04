d69c79c8dbb9baffc869503f7a5a60e0042714dc
NicollahSekete <nicollahtsekete@gmail.com>
2020-05-04 14:34:10 +0100

    first may commit

--- six.js
+++ six.js
@@ -1,1 +1,15 @@
 //Check balance and cry at the amount.
+const checkBalance=(pin)=>{
+    pin = prompt("enter your pin.") 
+    if (parseInt == "1235") {
+      alert("Correct pin. ");}
+      
+     balance= prompt("would you like to check balance??")
+         if(balance=="yes"){
+         alert("current balance is £7")
+        } else {
+            alert("Goodbye")}
+
+        }
+     
+    checkBalance()
